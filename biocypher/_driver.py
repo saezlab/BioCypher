c9bf5184421abfa47bea88f2d1c0ef240313acf2
bnymnsen <106750021+bnymnsen@users.noreply.github.com>
2022-10-03 03:03:00 +0300

    add wipe parameter for admin-import

--- biocypher/_driver.py
+++ biocypher/_driver.py
@@ -111,7 +111,7 @@ class Driver(neo4j_utils.Driver):
         self.csv_delim = delimiter or _config('csv_delimiter')
         self.csv_adelim = array_delimiter or _config('csv_array_delimiter')
         self.csv_quote = quote_char or _config('csv_quote_char')
-
+        self.wipe = wipe
         self.skip_bad_relationships = skip_bad_relationships
         self.skip_duplicate_nodes = skip_duplicate_nodes
 
@@ -588,6 +588,7 @@ class Driver(neo4j_utils.Driver):
                 db_name=db_name or self.current_db,
                 skip_bad_relationships=self.skip_bad_relationships,
                 skip_duplicate_nodes=self.skip_duplicate_nodes,
+                wipe=self.wipe,
             )
 
     def start_bl_adapter(self):
