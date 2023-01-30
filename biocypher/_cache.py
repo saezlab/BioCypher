5430002ea392e6fcff535fa5f666f230055c83fd
deeenes <turei.denes@gmail.com>
2023-01-30 18:10:01 +0100

    trivial issues in new cache access functions

--- biocypher/_cache.py
+++ biocypher/_cache.py
@@ -19,6 +19,7 @@ import os
 import hashlib
 import json
 from typing import Any
+import pickle
 
 import appdirs
 
@@ -48,7 +49,7 @@ def cache_key(*args: Any) -> str:
 
         pass
 
-    return hashlib.md5sum(json.dumps(args)).hexdigest()
+    return hashlib.md5(json.dumps(args).encode('utf-8')).hexdigest()
 
 
 def cache_dir() -> str:
@@ -59,12 +60,23 @@ def cache_dir() -> str:
     return _config.config('cachedir') or appdirs.user_cache_dir('biocypher')
 
 
-def cache_path(cachedir: str | None = None, *key) -> str:
+def ensure_cachedir(cachedir: str | None = None) -> str:
+    """
+    Makes sure the cache directory exists.
+    """
+
+    cachedir = cachedir or cache_dir()
+    os.makedirs(cachedir, exist_ok = True)
+
+    return cachedir
+
+
+def cache_path(*key, cachedir: str | None = None) -> str:
     """
     Path to a cache item.
     """
 
-    cachedir = cachedir or cache_dir()
+    cachedir = ensure_cachedir(cachedir)
     fname = cache_key(*key)
 
     return os.path.join(cachedir, fname)
@@ -75,7 +87,8 @@ def save(obj: Any, *key: Any, cachedir: str | None = None):
     Save an object to the cache.
     """
 
-    path = cache_path(cachedir = cachedir, *key)
+    cachedir = ensure_cachedir(cachedir)
+    path = cache_path(*key, cachedir = cachedir)
 
     _logger.logger.info(f'Saving object into `{path}`.')
 
@@ -91,7 +104,8 @@ def load(*key: Any, cachedir: str | None = None) -> Any | None:
     Load an object from the cache.
     """
 
-    path = cache_path(cachedir = cachedir, *key)
+    cachedir = ensure_cachedir(cachedir)
+    path = cache_path(*key, cachedir = cachedir)
 
     if os.path.exists(path):
 
